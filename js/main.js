20717db197fb0491708d706ddb31fd68991655df
SamuelRiveros <156517817+SamuelRiveros@users.noreply.github.com>
2024-03-21 11:18:08 -0500

    feat: :sparkles: retoques finales hechos, el js funciona correctamente

--- js/main.js
+++ js/main.js
@@ -2,3 +2,13 @@ function change() {
     var btn = document.getElementById("menucel");
     btn.classList.toggle("visible");
 }
+
+function cerrarMenu() {
+    var btn = document.getElementById("menucel");
+    btn.classList.remove("visible");
+}
+
+var enlacesMenu = document.querySelectorAll(".menucel a");
+enlacesMenu.forEach(function(enlace) {
+    enlace.addEventListener("click", cerrarMenu);
+});
