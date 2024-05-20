6d23a3a53c30f04a9901a2897a8121db649fa881
SamuelRiveros <156517817+SamuelRiveros@users.noreply.github.com>
2024-05-20 17:44:40 -0500

    feat: :construction: progreso en js para continuar los webcomponents

---
+++ src/js/components/products.js
@@ -0,0 +1,78 @@
+import { LitElement, html, css } from "lit";
+
+export class productos extends LitElement {
+    constructor() {
+        super()
+
+    }
+
+
+    static styles= css`
+        .card .description {
+            background-color: var(--color-gris);
+            border-bottom-left-radius: 10px;
+            border-bottom-right-radius: 10px;
+            height: 60px;
+            width: 250px;
+            display: flex;
+            flex-direction: row;
+            justify-content: space-evenly;
+            padding: 1em;
+            align-items: center;
+        }
+        
+        .card .description p {
+            font-size: 14px;
+            color: var(--color-blanco);
+        }
+        
+        .card .description .principal{
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        }
+        
+        
+        .card img {
+            max-height: 245px;
+            object-fit: contain;
+            padding: 5px;
+            -webkit-user-drag: none;
+        } 
+
+        .agregar {
+            border: none;
+            border-radius: 10px;
+            height: 30px;
+            width: 100px;
+            transition: all 0.3s;
+        }
+        
+        .agregar:hover {
+            cursor: pointer;
+            color: var(--color-blanco);
+            background-color: var(--color-grisclaro);
+            box-shadow: 0 0 20px #000;
+            transition: 0.3s;
+        }
+    `
+    render(){
+
+
+    return html`
+        <div class="card camisetas">
+            <img src="img/defaultshirt.png">
+            <div class="description">
+                <div class="principal">
+                    <p>Camiseta Negra Basica</p>
+                    <p>$15.000</p>
+                </div>
+                <button class="agregar">Agregar</button>
+            </div>
+        </div>
+        `
+    }
+
+}
+
+customElements.define("productos", productos);
